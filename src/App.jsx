6b48a3bae9e1e86613b835b542b24c33f276d0cd
Bry@n A. Miranda <100953022+bryanmiranda138@users.noreply.github.com>
2026-08-25 10:44:06 -0600

    se agregga icono

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,16 @@ import Configuracion from './pages/Configuracion';
 
 // Layout que incluye la barra lateral para las páginas del Dashboard
 function DashboardLayout() {
-  // 👇 2. Extraemos las variables del contexto
+  //2. Extraemos las variables del contexto
   const { temaOscuro, toggleTema } = useTheme(); 
 
   return (
     <div className="flex min-h-screen bg-gray-50 dark:bg-gray-950 transition-colors duration-300 w-full overflow-x-hidden">
-      
-      {/* 👇 3. Se las inyectamos al Sidebar */}
+      {/* 3. Se las inyectamos al Sidebar */}
       <Sidebar temaOscuro={temaOscuro} toggleTema={toggleTema} />
-      
       <main className="flex-1 min-w-0 md:ml-64 pt-16 md:pt-0 min-h-screen flex flex-col">
         <Outlet />
       </main>
-      
     </div>
   );
 }
